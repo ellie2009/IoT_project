acd46395342cf8fccbe721c9f1e2f7501671b252
Elisabeth Haubold <ejhaubold@gmailcom>
2022-10-23 18:06:40 +0100

    Moved Particle login into separate obtainLoginToken function

--- routes/particle.js
+++ routes/particle.js
@@ -9,16 +9,27 @@ var token;
 const email = process.env.PARTICLE_EMAIL;
 const password = process.env.PARTICLE_PASSWORD;
 
-router.get("/", function(req, res, next) {
-  particle.login({username: email, password: password}).then(
-      function(data) {
-        token = data.body.access_token;
-        console.log(token)
-      },
-      function (err) {
-        console.log('Could not log in.', err);
-      }
-    );
+var obtainLoginToken = async function(req, res){ 
+  var token = await particle.login({username: email, password: password}).then(
+    function(data) {
+      return data.body.access_token;
+    },
+    function (err) {
+      //console.log('Could not log in.', err);
+      return "error";
+    }
+  );
+
+  return token;
+};
+
+router.get("/", async function(req, res, next) {
+  const loginToken = await obtainLoginToken();
+  if(loginToken != "error"){
+    res.send('Token creation worked. Token is: ' + loginToken);
+  } else {
+    res.send("An error occurred.");
+  }
 });
 
 module.exports = router;
