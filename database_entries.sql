c5c6a534084638c0645de1ef7e6fd2e16148c447
Elisabeth Haubold <ejhaubold@gmailcom>
2022-10-29 20:01:00 +0100

    SQL file with basic queries added for manual testing

---
+++ database_entries.sql
@@ -0,0 +1,16 @@
+--insert data e.g.
+-- insert into bins (id_council, gps_north, gps_west, isFull) values ("mimi", "mimi", "mimi", 0);
+
+-- Update the 'isFull' field of an existing bin record
+-- call for example http://localhost:8000/bins/1
+
+-- insert into bins (id_council, gps_north, gps_west, isFull) values ("f9d0c4e2", "53°21\'16.712888\"N", "6°25\'14.374028\"W", 0);
+-- insert into bins (id_council, gps_north, gps_west, isFull) values ("95199f5a", "53°17\'17.813247\"N", "6°24\'17.343278\"W", 0);
+-- insert into bins (id_council, gps_north, gps_west, isFull) values ("45939cf1", "53°19\'21.802871\"N", "6°23\'44.063747\"W", 0);
+-- insert into bins (id_council, gps_north, gps_west, isFull) values ("ee786fd7", "53°19\'15.129405\"N", "6°23\'35.871881\"W", 0);
+-- insert into bins (id_council, gps_north, gps_west, isFull) values ("c8a03854", "53°21\'1.565115\"N", "6°22\'33.743399\"W", 0);
+-- insert into bins (id_council, gps_north, gps_west, isFull) values ("4270ed2f", "53°17\'36.672825\"N", "6°22\'9.525372\"W", 0);
+-- insert into bins (id_council, gps_north, gps_west, isFull) values ("16bffc1f", "53°17\'17.131598\"N", "6°21\'32.775739\"W", 0);
+-- insert into bins (id_council, gps_north, gps_west, isFull) values ("10df2f96", "53°16\'31.837955\"N", "6°20\'32.613444\"W", 0);
+-- insert into bins (id_council, gps_north, gps_west, isFull) values ("3182a616", "53°16\'30.388055\"N", "6°19\'47.767894\"W", 0);
+-- insert into bins (id_council, gps_north, gps_west, isFull) values ("6aee6851", "53°17\'18.978900\"N", "6°17\'56.27518\"W", 0);
